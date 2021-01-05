89cdc5182593cc297e4ce3e33f9354f1a734117d
Aleksi Immonen <aleksi.immonen@gmail.com>
2021-01-05 14:54:55 +0200

    korjaus

--- src/components/Services.js
+++ src/components/Services.js
@@ -16,13 +16,13 @@ const Services = () => {
                         </h1>
                         <div className="skills">
                             <p className="services-text">
-                                🚀 <span className="services">Create a growth machine:</span> I will gather and train an elite growth team in your organisation. In six months you will have a self-directed and effective growth team creating results.                     
+                                <span className="services">Create a growth machine:</span> I will gather and train an elite growth team in your organisation. In six months you will have a self-directed and effective growth team creating results.                     
                             </p>
                             <p className="services-text">
-                                🚀 <span className="services">What is happening in the cutting edge of internet business / indiehacking community:</span> I will get you up to date on what is happening in the most dynamic and creative part of the global economy.                  
+                                <span className="services">What is happening in the cutting edge of internet business / indiehacking community:</span> I will get you up to date on what is happening in the most dynamic and creative part of the global economy.                  
                             </p>
                             <p className="services-text">
-                                🚀 <span className="services">Growth Hacking Clinic:</span> I will get your team from best practices to unconventional solutions. Only for elite teams, capable of executing.                  
+                                <span className="services">Growth Hacking Clinic:</span> I will get your team from best practices to unconventional solutions. Only for experienced teams.                  
                             </p>
                         </div>
                         <h1 className="now-style2">
