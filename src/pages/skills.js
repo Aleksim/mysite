f7555d27efae80fce227a4bc769d1e5b7e36e6f4
Aleksi Immonen <aleksi.immonen@gmail.com>
2021-06-03 17:00:04 +0300

    uusi sivu

---
+++ src/pages/skills.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from "../components/Layout"
+import SEO from '../components/SEO'
+import Services from '../components/Services'
+
+
+const services = () => {
+    return (
+  <Layout>
+    <SEO title="Hire Me" />
+    <Services />
+  </Layout>
+    )
+}
+
+export default services
