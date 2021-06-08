142ed1a673affdd59d9acf143c9d67f29718f52d
Aleksi Immonen <aleksi.immonen@gmail.com>
2021-06-08 15:05:50 +0300

    korjauksia

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -45,7 +45,7 @@ const Skills = () => {
                                 <span className="year">Grew 2020-2021</span>: Grew was a lesson on "learning to fly in a free fall" 😉. Our journey started when we got Tempo-funding for our pointNG innovation. However, we realized quickly that in order to make pointNG produce money we would need to do A LOT of technical development and even still the product market fit was highly questionable and depended on e.g. how privacy law would develop in the future. However, we had time on our hands and a great team so we started hustling. Eventually, we ended up creating <span><a href="https://www.pointNG.io">pointNG</a></span>, <span><a href="https://www.consolechat.io">Consolechat </a></span>(trending in front page of Hackernews for 12 hours and 6500 users in first 24h), <span><a href="https://www.hacker-ai.io">Hacker AI </a></span> (260 upvotes and #3 Product of the Day), <span><a href="https://www.growthhackertools.io">Growthhackertools </a></span>(my quick side hustle), <span><a href="https://www.grew.ai">Grew.ai </a></span> (a generic email subject line prediction tool), <span><a href="https://www.grewai.com">GrewAI </a></span>(our first GPT-3 application with Mailchimp integration) and last but not least <span><a href="https://www.grewai.com">Superlines </a></span> (A GPT-3 application). Before this project I had never built web crawlers, created animations, launched in product hunt, trended in Hackernews, created cap tables and financial projections, applied for patents in US and EU and so on.. Learned amazingly much in 7 months.
                             </p>
                             <p className="now-text2">
-                                <span className="year">Selaamo 2018-2019</span>: Selaamo was a big challenge from growth and leadership perspective: how to build a complex, two sided platform with physical dimensions, with no money and no full-time employees. Throughout the project my main focus was to figure out how we make Selaamo a success while not burning anyone out or losing friendships. Our plans worked really well and we managed to get over 4000 users to the platform in the first few months and lots of sales happening on the platform. Things started to go downhill as we couldn't keep growing the user base quickly enough. This was due to 1) no VC was interested in funding book recycling 2) no money to set up customer acquisition programs 3) Team started to get weary after 1 year of working on weekends and after work. Learned a lot, especially about leadership.
+                                <span className="year">Selaamo 2018-2019</span>: Selaamo was a big challenge from growth and leadership perspective: how to build a complex, two sided platform with physical dimensions, with no money and no full-time employees. Throughout the project my main focus was to figure out how we can make Selaamo a success while not burning anyone out or losing friendships. Our plans worked really well and we managed to get over 4000 users to the platform in the first few months and lots of sales happening on the platform. Things started to go downhill as we couldn't keep growing the user base quickly enough. This was due to 1) no VC was interested in funding book recycling 2) no money to set up customer acquisition programs 3) Team started to get weary after 1 year of working on weekends and after work. Learned a lot, especially about leadership.
                             </p>
                             <p className="now-text2">
                                 <span className="year">WhisprApp 2013-2014</span>: WhisprApp was my first taste of SaaS / mobile app / product development while I was studying marketing in Turku School of Economics. WhisprApp was exactly like the now famous 'Jodel' application. I did not know any developers at that time but luckily met the cool guys: Timo and Larisa in one hackathon. We eventually built the service for Windows Phone 8 and had some excited users in the university. We participated in the Summer of start-ups arranged by Boost Turku. Every start up coach hated our idea and wanted to change it 😂. Learned valuable lessons with the whole experience.
@@ -66,7 +66,7 @@ const Skills = () => {
                                         return (
                                             <li key={`pdf-${index}`}>
                                             <a href={file.node.publicURL} download>
-                                                Read from here
+                                                Check it out
                                             </a>
                                             </li>
                                         )
@@ -74,16 +74,16 @@ const Skills = () => {
                                     </ul>
                             </p>
                             <p className="now-text2">
-                                <span className="year">Blog post about the future of "growth hacking" written in 2019</span>: 
-                                <p className="now-text2">Based on what I've seen and experienced, I still think that the biggest force multiplier a marketer can have is the ability to code. Read the blog post <span><a href="https://www.linkedin.com/pulse/future-growth-hacking-aleksi-immonen/">here</a></span>.</p>
+                                <span className="year">Blog post about the "future of growth hacking" written in 2019</span>: 
+                                <span><a href="https://www.linkedin.com/pulse/future-growth-hacking-aleksi-immonen/"> Check it out</a></span>.
                             </p>
                             <p className="now-text2">
                                 <span className="year">Article in Hackernoon</span>: 
-                                Read the blog post <span><a href="https://hackernoon.com/turning-users-invisible-edge-machine-learning-in-privacy-first-location-detection-k1a34tj">here</a></span> (we write in symbiosis with Kimmo 😄).
+                                <span><a href="https://hackernoon.com/turning-users-invisible-edge-machine-learning-in-privacy-first-location-detection-k1a34tj"> Check it out</a></span> (we write in symbiosis with Kimmo 😄).
                                 
                             </p>
                             <p className="now-text2">
-                                <span className="year"><a href="/posts"> Check my blog</a></span> 
+                                <span className="year">Check my blog:<a href="/posts"> here</a></span> 
                             </p>
                             <div className="section-item">
                                 <h2 className="section-topic2">
@@ -95,7 +95,7 @@ const Skills = () => {
                             </p>
                             <p className="now-text2">
                                 <span className="year">Python</span>:
-                                I have basic Python Skills. I can build Scrapy / Splash spiders and crawlers to automatically collect data and make them do work for me. I have also built data connector scripts and various data spinning/engineering scripts.
+                                I have basic Python Skills. I can build Scrapy / Splash spiders and crawlers to automatically collect data and make them do work for me. I have also built data connector scripts and various data spinning/engineering scripts (pandas, numpy).
                             </p>
                             <p className="now-text2">
                                 <span className="year">Javascript</span>: I can do basic stuff with Javascript as well. I've built two simple ReactNative apps using context and hooks. <span><a href="https://github.com/Aleksim">I have some of my projects in Github</a></span>. I've also built one React web app using redux. <span><a href="https://www.growthhackertools.io">Growth Hacker Tools</a></span> is a Gatsby app with dynamic react app embedded into it and buch of scrapers running in the background.
