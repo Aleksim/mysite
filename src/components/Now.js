b05f960cd14c2fe7c57a2520722830de3c63bc26
Aleksi Immonen <aleksi.immonen@gmail.com>
2021-08-28 10:04:53 +0300

    now kuvaus

--- src/components/Now.js
+++ src/components/Now.js
@@ -6,63 +6,63 @@ const Now = () => {
     return (
         <div className="section">
             <div className="container movetocenter">
-               
-                    <div className="section-style">
-                        <h1 className="now-style">
+
+                <div className="section-style">
+                    <h1 className="now-style">
                         Hello 👋
-                        </h1>
-                        <div className="section-item">
-                            <h2 className="section-topic">
+                    </h1>
+                    <div className="section-item">
+                        <h2 className="section-topic">
                                 Now>>
-                            </h2>
-                        </div>
-                        <p className="now-text">
-                            Full-time windsurfer looking for the next challenge.
-                        </p>
-                        <p className="now-text2">
-                            But always sharpening my marketing / hacking skills and updating <span><a href="https://www.growthhackertools.io">Growth Hacker Tools</a></span>.
-                        </p>
+                        </h2>
+                    </div>
+                    <p className="now-text">
+                        Head of Ecommerce at <span><a href="https://www.lumise.fi">Lumise</a></span>.
+                    </p>
+                    <p className="now-text2">
+                        But always sharpening my marketing / hacking skills and updating <span><a href="https://www.growthhackertools.io">Growth Hacker Tools</a></span>.
+                    </p>
 
+                    <div className="section-item">
+                        <h2 className="section-topic">
+                                Previously>>
+                        </h2>
+                    </div>
+                    <p className="now-text">
+                        Co-founder at <span>Grew</span> (<span className="year">2020-2021</span>). In 7 months of intensive work we created: <span><a href="https://www.pointng.io">pointNG</a></span>, <span><a href="https://www.superlines.io">Superlines</a></span>, <span><a href="https://www.grewai.com">Grew AI</a></span>, <span><a href="https://www.consolechat.io">Consolechat</a></span>, <span><a href="https://www.hacker-ai.com">Hacker AI</a></span> and lots of other smaller stuff.
+                    </p>
+                    <p className="now-text2">
+                        Head of Growth at a <span><a href="https://www.musopia.net">mobile app company</a></span> (<span className="year">2020</span>).
+                    </p>
+                    <p className="now-text2">
+                        <span><a href="https://www.avaus.com">Martech + management consulting</a></span> and founder of two mobile app companies (<span className="year">2012-2019</span>).
+                    </p>
+                    <div className="skills">
                         <div className="section-item">
                             <h2 className="section-topic">
-                                Previously>>
+                                    Skills>>
                             </h2>
                         </div>
-                        <p className="now-text">
-                            Co-founder at <span>Grew</span> (<span className="year">2020-2021</span>). In 7 months of intensive work we created: <span><a href="https://www.pointng.io">pointNG</a></span>, <span><a href="https://www.superlines.io">Superlines</a></span>, <span><a href="https://www.grewai.com">Grew AI</a></span>, <span><a href="https://www.consolechat.io">Consolechat</a></span>, <span><a href="https://www.hacker-ai.com">Hacker AI</a></span> and lots of other smaller stuff.
+                        <p className="skill-text">
+                            🚀 Setting up and leading growth teams from start-up companies to global corporations.
                         </p>
-                        <p className="now-text2">
-                            Head of Growth at a <span><a href="https://www.musopia.net">mobile app company</a></span> (<span className="year">2020</span>).
+                        <p className="skill-text">
+                            🤖 <span><a href="https://github.com/aleksim">Self-learned software developer</a></span> with focus on web (apps and scraping), data engineering and light mobile full-stack apps.
+                            Javascript/React/ReactNative/Gatsbyjs + Python.
                         </p>
-                        <p className="now-text2">
-                            <span><a href="https://www.avaus.com">Martech + management consulting</a></span> and founder of two mobile app companies (<span className="year">2012-2019</span>).
+                        <p className="skill-text">
+                            🦾 Extensive experience from using and coordinating the use of martech tools and techniques. ASO, SEO, Automation, Paid, Organic, Referral, SDK's, attribution, Adobe, SFMC and what have you.
                         </p>
-                        <div className="skills">
-                            <div className="section-item">
-                                <h2 className="section-topic">
-                                    Skills>>
-                                </h2>
-                            </div>
-                            <p className="skill-text">
-                                🚀 Setting up and leading growth teams from start-up companies to global corporations.                     
-                            </p>
-                            <p className="skill-text">
-                                🤖 <span><a href="https://github.com/aleksim">Self-learned software developer</a></span> with focus on web (apps and scraping), data engineering and light mobile full-stack apps. 
-                                Javascript/React/ReactNative/Gatsbyjs + Python.                    
-                            </p>
-                            <p className="skill-text">
-                                🦾 Extensive experience from using and coordinating the use of martech tools and techniques. ASO, SEO, Automation, Paid, Organic, Referral, SDK's, attribution, Adobe, SFMC and what have you.                
-                            </p>
 
-                            <p className="skill-text">
-                            🎓 Master's degree in marketing from University of Turku. Specialization in platform service dynamics and media cost effectiveness. 
-                                Graduated top of my class.
-                            </p>
-                            <p className="skill-text">
-                                🧐 Designer (Photoshop, Illustrator), <span><a href="https://youtu.be/cwrFGTcYoSM">animator(After Effects)</a></span> and a drone pilot.
-                            </p>
-                        </div>
+                        <p className="skill-text">
+                            🎓 Master's degree in marketing from University of Turku. Specialization in platform service dynamics and media cost effectiveness.
+                            Graduated top of my class.
+                        </p>
+                        <p className="skill-text">
+                            🧐 Designer (Photoshop, Illustrator), <span><a href="https://youtu.be/cwrFGTcYoSM">animator(After Effects)</a></span> and a drone pilot.
+                        </p>
                     </div>
+                </div>
             </div>
         </div>
     )
