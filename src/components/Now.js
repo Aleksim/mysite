9aa627b6a862ec100e244c4add404d54756b63fb
Aleksi Immonen <aleksi.immonen@gmail.com>
2022-05-07 21:04:50 +0300

    duunie

--- src/components/Now.js
+++ src/components/Now.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaDiscord, FaTwitter, FaLinkedinIn, FaGithub} from "react-icons/fa"
+import { FaDiscord, FaTwitter, FaLinkedinIn, FaGithub, FaRecycle} from "react-icons/fa"
 import "../css/main.css"
 
 
@@ -10,10 +10,9 @@ const Now = () => {
                 <div className="section-style">
                     <div className="section-item">
 
-                            <p className='about'>My name is Aleksi Immonen and I am a digital innovator, product owner, performance marketer, former martech-consultant, growth hacker and so on. <span className='teamwork-emphasis'>I can wear many hats</span>.</p>
-                            <p className='about2'>I'm from Helsinki, Finland and currently scheming my next adventures. On this site you can read about my skills, thoughts and about the projects I have been working on!</p>
+                            <p className='about2'>Currently working as a <span className='teamwork-emphasis'>Performance Marketing Manager at <span><a href="https://www.rentle.io">Rentle</a><FaRecycle size={12} style={{marginLeft:"3px", marginTop:"", color:"#1DA1F2"}}/></span></span>.</p>
 
-                            {/* <p className='about2'>On this site you can read about my skills, thoughts and about the projects I have been working on! </p> */}
+                            <p className='about2'>On this site you can read about my skills, thoughts and about the projects I have been working on! </p>
                             <p className='about2'>You can also find me on <span><a href="https://twitter.com/aleksimmonen"><FaTwitter size={15} style={{marginLeft:"3px", color:"#1DA1F2"}}/></a>, <a href="https://github.com/Aleksim"><FaGithub size={15} style={{marginLeft:"3px", color:"#000"}}/></a>, or <a href="https://www.linkedin.com/in/aleksi-immonen-31132233/"><FaLinkedinIn size={15} style={{marginLeft:"", color:"#2867B2"}}/>.</a></span> </p>
                             
                        
