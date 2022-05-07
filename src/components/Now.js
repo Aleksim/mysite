c5afb1fd921a4f61e8d8984a87f8700f7e2ce46c
Aleksi Immonen <aleksi.immonen@gmail.com>
2022-05-07 21:23:39 +0300

    f

--- src/components/Now.js
+++ src/components/Now.js
@@ -9,9 +9,7 @@ const Now = () => {
             <div className="container is-max-desktop">
                 <div className="section-style">
                     <div className="section-item">
-
                             <p className='about2'>Currently working as a <span className='teamwork-emphasis'>Performance Marketing Manager at <span><a target="_blank" href="https://www.rentle.io">Rentle</a><FaRecycle size={12} style={{marginLeft:"3px", marginTop:"", color:"#1DA1F2"}}/></span></span>.</p>
-
                             <p className='about2'>On this site you can read about my skills, thoughts and about the projects I have been working on! </p>
                             <p className='about2'>You can also find me on <span><a href="https://twitter.com/aleksimmonen"><FaTwitter size={15} style={{marginLeft:"3px", color:"#1DA1F2"}}/></a>, <a href="https://github.com/Aleksim"><FaGithub size={15} style={{marginLeft:"3px", color:"#000"}}/></a>, or <a href="https://www.linkedin.com/in/aleksi-immonen-31132233/"><FaLinkedinIn size={15} style={{marginLeft:"", color:"#2867B2"}}/>.</a></span> </p>
                             
